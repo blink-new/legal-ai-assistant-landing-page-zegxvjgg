4c1075bc9cd77458d89c56ae981e9f7632da2bcf
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-27 11:38:25 +0000

    Fix Font Import Paths

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './index.css'
-import '@fontsource/inter/variable.css'
+import '@fontsource/inter'
 import '@fontsource/playfair-display/400.css'
 import '@fontsource/playfair-display/700.css'
 
